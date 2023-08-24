e000feed9ff4d03b8c10b638b523e8b027a33f4e
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-24 03:50:45 +0000

    chore(example): run `fluentci init -t gradle`

---
+++ example/.fluentci/src/dagger/jobs.ts
@@ -0,0 +1,165 @@
+import Client from "@dagger.io/dagger";
+import { withDevbox } from "https://deno.land/x/nix_installer_pipeline@v0.3.6/src/dagger/steps.ts";
+
+export enum Job {
+  build = "build",
+  test = "test",
+  check = "check",
+}
+
+export const build = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+
+  const baseCtr = withDevbox(
+    client
+      .pipeline(Job.build)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec([
+        "apk",
+        "add",
+        "bash",
+        "curl",
+        "wget",
+        "unzip",
+        "git",
+        "libstdc++",
+        "zlib",
+        "gcompat",
+      ])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withExec(["sh", "-c", "ls -ltr /nix"])
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  );
+
+  const ctr = baseCtr
+    .withMountedCache("/app/.gradle", client.cacheVolume("gradle-cache"))
+    .withMountedCache("/root/.gradle", client.cacheVolume("gradle-root-cache"))
+    .withMountedCache("/app/app/build", client.cacheVolume("gradle-app-build"))
+    .withDirectory("/app", context, {
+      exclude: ["build", ".gradle", "app/build", ".devbox", ".fluentci"],
+    })
+    .withWorkdir("/app")
+    .withExec(["chmod", "+x", "./gradlew"])
+    .withExec(["sh", "-c", "ls -ltr /nix"])
+    .withExec(["nix", "--version"])
+    .withExec(["sh", "-c", "devbox run -- ./gradlew build"]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const test = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+
+  const baseCtr = withDevbox(
+    client
+      .pipeline(Job.test)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec([
+        "apk",
+        "add",
+        "bash",
+        "curl",
+        "wget",
+        "unzip",
+        "git",
+        "libstdc++",
+        "zlib",
+        "gcompat",
+      ])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withExec(["sh", "-c", "ls -ltr /nix"])
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  );
+
+  const ctr = baseCtr
+    .withMountedCache("/app/.gradle", client.cacheVolume("gradle-cache"))
+    .withMountedCache("/root/.gradle", client.cacheVolume("gradle-root-cache"))
+    .withMountedCache("/app/app/build", client.cacheVolume("gradle-app-build"))
+    .withDirectory("/app", context, {
+      exclude: ["build", ".gradle", "app/build", ".devbox", ".fluentci"],
+    })
+    .withWorkdir("/app")
+    .withExec(["chmod", "+x", "./gradlew"])
+    .withExec(["sh", "-c", "ls -ltr /nix"])
+    .withExec(["nix", "--version"])
+    .withExec(["sh", "-c", "devbox run -- ./gradlew test"]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export const check = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+
+  const baseCtr = withDevbox(
+    client
+      .pipeline(Job.build)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec([
+        "apk",
+        "add",
+        "bash",
+        "curl",
+        "wget",
+        "unzip",
+        "git",
+        "libstdc++",
+        "zlib",
+        "gcompat",
+      ])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withExec(["sh", "-c", "ls -ltr /nix"])
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  );
+
+  const ctr = baseCtr
+    .withMountedCache("/app/.gradle", client.cacheVolume("gradle-cache"))
+    .withMountedCache("/root/.gradle", client.cacheVolume("gradle-root-cache"))
+    .withMountedCache("/app/app/build", client.cacheVolume("gradle-app-build"))
+    .withDirectory("/app", context, {
+      exclude: ["build", ".gradle", "app/build", ".devbox", ".fluentci"],
+    })
+    .withWorkdir("/app")
+    .withExec(["chmod", "+x", "./gradlew"])
+    .withExec(["sh", "-c", "ls -ltr /nix"])
+    .withExec(["nix", "--version"])
+    .withExec(["sh", "-c", "devbox run -- ./gradlew check"]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export type JobExec = (
+  client: Client,
+  src?: string
+) =>
+  | Promise<void>
+  | ((
+      client: Client,
+      src?: string,
+      options?: {
+        ignore: string[];
+      }
+    ) => Promise<void>);
+
+export const runnableJobs: Record<Job, JobExec> = {
+  [Job.build]: build,
+  [Job.test]: test,
+  [Job.check]: check,
+};
+
+export const jobDescriptions: Record<Job, string> = {
+  [Job.build]: "Build the project",
+  [Job.test]: "Run the tests",
+  [Job.check]: "Check the project",
+};
