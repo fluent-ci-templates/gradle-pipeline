9cadeca87e63dd6b114f638a143be528d71c9a42
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-25 06:03:44 +0000

    update ci.ts

--- ci.ts
+++ ci.ts
@@ -1,12 +1,16 @@
-const command = new Deno.Command(Deno.execPath(), {
-  args: [
-    "run",
-    "-A",
-    "--import-map=https://deno.land/x/base_pipeline/import_map.json",
-    "https://deno.land/x/base_pipeline/src/dagger/runner.ts",
-  ],
-});
+import Client, { connect } from "https://sdk.fluentci.io/v0.1.9/mod.ts";
+import {
+  check,
+  test,
+  build,
+} from "https://pkg.fluentci.io/gradle_pipeline@v0.3.1/mod.ts";
 
-const { stdout } = await command.output();
+function pipeline(src = ".") {
+  connect(async (client: Client) => {
+    await check(client, src);
+    await test(client, src);
+    await build(client, src);
+  });
+}
 
-console.log(new TextDecoder().decode(stdout));
+pipeline();
